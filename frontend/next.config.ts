b130eb93c4fd7852a97030c355eeb8599a459865
杠葉 梛 <unipy46@gmail.com>
2025-12-20 16:18:57 +0900

    Resolve next.config.ts merge conflict

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
-=======
   reactCompiler: true,
->>>>>>> yozure
 };
 
 export default nextConfig;
